d288485a6130895ecc425817c47ae72c1b1fa8dc
John Rake <john.rake12@gmail.com>
2016-01-10 12:36:30 -0600

    enable a js class on the document element

---
+++ assets/js/enable-javascript.js
@@ -0,0 +1,13 @@
+/*
+ * Regex Description
+ *
+ * (?:^|\s) # match the start of the string, or any single whitespace character
+ *
+ * (?!\S)   # negative lookahead to verify the above is the whole classname
+ *          # ensures there is no non-space character following
+ *          # (i.e. must be end of string or a space)
+ */
+document.addEventListener("DOMContentLoaded", function(event) {
+  // mark document as js enabled
+  document.documentElement.className = document.documentElement.className.replace( /(?:^|\s)no-js(?!\S)/g , 'js' )
+});

d3b12551c6c2fc0c66728541be97d390167a83b7
John Rake <john.rake12@gmail.com>
2016-01-10 11:31:10 -0600

    Add default experiation for jwt tokens.

--- config/services/cipher.js
+++ config/services/cipher.js
@@ -2,7 +2,8 @@ export default {
   services: {
     cipher: {
       jwt: {
-        secretKey: '12fc3531c53ebe558747624b486cded9948e30f3bcbb146629838f1f940e45ac'
+        secretKey: '12fc3531c53ebe558747624b486cded9948e30f3bcbb146629838f1f940e45ac',
+        expiresIn: 60 * 60 * 24
       }
     }
   }

7936f401192ad7a3544c03e3cbb298727a71f801
John Rake <john.rake12@gmail.com>
2016-01-10 13:14:04 -0600

    Enable run time js indication

--- assets/js/enable-javascript.js
+++ assets/js/enable-javascript.js
@@ -7,7 +7,7 @@
  *          # ensures there is no non-space character following
  *          # (i.e. must be end of string or a space)
  */
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
   // mark document as js enabled
-  document.documentElement.className = document.documentElement.className.replace( /(?:^|\s)no-js(?!\S)/g , 'js' )
+  document.documentElement.className = document.documentElement.className.replace(/(?:^|\s)no-js(?!\S)/g, 'js')
 });

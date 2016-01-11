29e3a35560b533e84645f693dd138d16ee98b458
John Rake <john.rake12@gmail.com>
2016-01-11 13:20:10 -0600

    Secure AWS S3 credentials to be environment based

--- api/controllers/FilesController.js
+++ api/controllers/FilesController.js
@@ -9,6 +9,8 @@ import AWS from 'aws-sdk';
 import skipperS3 from 'skipper-s3';
 
 export function create(req, res, next) {
+  // @TODO: if something is wrong with the configuration then the whole
+  // application dies. Should we be catching or be using domains?
   req.file('song').upload({
     adapter: skipperS3,
     key: sails.config.aws.s3.accessKeyId,
@@ -19,7 +21,7 @@ export function create(req, res, next) {
       return res.serverError(error);
     }
 
-    let assets = updoadedFiles.map(function(file) {
+    let assets = updoadedFiles.map(function (file) {
       return {
         userId: req.user.id,
         fileName: file.filename,
@@ -31,13 +33,13 @@ export function create(req, res, next) {
     });
 
     ArtistsAssets.create(assets)
-    .then(function() {
-      sails.log.verbose("successfully added assets");
-      return res.redirect("/dashboard");
-    })
-    .catch((e) => {
-      sails.log.error(e);
-      return res.serverError();
-    });
+      .then(function () {
+        sails.log.verbose("successfully added assets");
+        return res.redirect("/dashboard");
+      })
+      .catch((e) => {
+        sails.log.error(e);
+        return res.serverError();
+      });
   });
 }
